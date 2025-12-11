8f741c618ef84169e7425f4ffce28a7475e15b3b
Yassar2 <yassarmalik4@gmail.com>
2025-12-11 13:15:41 +0700

    update fetch

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ document.getElementById("uploadBtn").addEventListener("click", async () => {
     const resultDiv = document.getElementById("result");
     resultDiv.innerHTML = "<p>Checking...</p>";
 
-    const response = await fetch("http://localhost:5000/predict", {
+    const response = await fetch("https://be-plagiarism-checker-production.up.railway.app/predict", {
         method: "POST",
         body: formData
     });
